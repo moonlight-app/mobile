7b0ba1918551871ad9938071e03fd751a0ddc592
Kirill <99124227+Kupidji@users.noreply.github.com>
2024-10-31 21:26:18 +0500

    Update MoonlightNavHost

--- app/src/main/java/ru/moonlight/mobile/MoonlightNavHost.kt
+++ app/src/main/java/ru/moonlight/mobile/MoonlightNavHost.kt
@@ -5,7 +5,14 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import ru.moonlight.feature_auth.sign_in.navigation.SignInRoute
 import ru.moonlight.feature_auth.sign_in.navigation.signInScreen
+import ru.moonlight.feature_auth.sign_up.confirm_code.navigation.confirmCodeScreen
+import ru.moonlight.feature_auth.sign_up.confirm_code.navigation.navigateToConfirmCode
+import ru.moonlight.feature_auth.sign_up.registration.navigation.navigateToRegistration
+import ru.moonlight.feature_auth.sign_up.registration.navigation.registrationScreen
+import ru.moonlight.feature_auth.sign_up.registration_complete.navigation.navigateToRegistrationComplete
+import ru.moonlight.feature_auth.sign_up.registration_complete.navigation.registrationCompleteScreen
 import ru.moonlight.feature_cart.navigation.cartScreen
 import ru.moonlight.feature_catalog.navigation.CatalogRoute
 import ru.moonlight.feature_catalog.navigation.catalogScreen
@@ -21,7 +28,17 @@ fun MoonlightNavHost(
         startDestination = CatalogRoute,
     ) {
         signInScreen(
-            onAuthorizeClick = {},
+            onAuthorizeClick = { navController.navigateUp() },
+            onRegistrationClick = { navController.navigateToRegistration() },
+        )
+        registrationScreen(
+            onCreateAccountClick = { navController.navigateToConfirmCode() },
+        )
+        confirmCodeScreen(
+            onContinueClick = { navController.navigateToRegistrationComplete() }
+        )
+        registrationCompleteScreen(
+            onGetStartClick = {  } //TODO Add update profileScreen logic
         )
         catalogScreen()
         cartScreen()
