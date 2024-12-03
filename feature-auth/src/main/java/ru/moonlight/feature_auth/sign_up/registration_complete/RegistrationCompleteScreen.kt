5ae5bbed8781c9570b8fd2a9f28e1e59d95544a1
Kirill <99124227+Kupidji@users.noreply.github.com>
2024-12-03 22:20:38 +0500

    Implement registration complete screen

--- feature-auth/src/main/java/ru/moonlight/feature_auth/sign_up/registration_complete/RegistrationCompleteScreen.kt
+++ feature-auth/src/main/java/ru/moonlight/feature_auth/sign_up/registration_complete/RegistrationCompleteScreen.kt
@@ -1,17 +1,14 @@
 package ru.moonlight.feature_auth.sign_up.registration_complete
 
-import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import ru.moonlight.feature_auth.R
 import ru.moonlight.theme.MoonlightTheme
@@ -23,37 +20,27 @@ fun RegistrationCompleteScreen(
     onGetStartClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val context = LocalContext.current
-
-    Box(
+    Column(
         modifier = modifier
-            .fillMaxSize()
-            .background(color = MoonlightTheme.colors.background),
-        contentAlignment = Alignment.TopCenter,
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .padding(top = MoonlightTheme.dimens.paddingFromEdges * 6)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(
+            MoonlightTheme.dimens.paddingBetweenComponentsBigVertical,
+            Alignment.CenterVertically,
+        ),
     ) {
-        Column(
+        TextAuthComponent(
+            subTitleText = stringResource(R.string.accountComplete),
+            bodyText = stringResource(R.string.accountWasCreatedAndConfirmed),
+        )
+        ButtonComponent(
             modifier = Modifier
-                .padding(top = MoonlightTheme.dimens.paddingFromEdges * 7)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(
-                MoonlightTheme.dimens.paddingBetweenComponentsBigVertical,
-                Alignment.CenterVertically
-            ),
-        ) {
-            TextAuthComponent(
-                subTitleText = stringResource(R.string.accountComplete),
-                bodyText = stringResource(R.string.accountWasCreatedAndConfirmed),
-            )
-            ButtonComponent(
-                modifier = Modifier
-                    .fillMaxWidth(0.55f),
-                onClick = {
-                    Toast.makeText(context, "Navigate back to profile", Toast.LENGTH_SHORT).show()
-                    onGetStartClick()
-                },
-                text = stringResource(R.string.getStart),
-            )
-        }
+                .fillMaxWidth(0.55f),
+            onClick = { onGetStartClick() },
+            text = stringResource(R.string.getStart),
+        )
     }
 }
