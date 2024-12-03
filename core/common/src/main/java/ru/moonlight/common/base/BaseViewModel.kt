874ec9f315385ebbe4fad6f99f928c225c418927
Kirill <99124227+Kupidji@users.noreply.github.com>
2024-12-03 22:11:37 +0500

    Update BaseViewModel

--- core/common/src/main/java/ru/moonlight/common/base/BaseViewModel.kt
+++ core/common/src/main/java/ru/moonlight/common/base/BaseViewModel.kt
@@ -1,6 +1,8 @@
 package ru.moonlight.common.base
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
 
@@ -9,4 +11,12 @@ open class BaseViewModel<State: Any, SideEffects: Any>(
 ) : ViewModel(), ContainerHost<State, SideEffects> {
 
     override val container = container<State, SideEffects>(initialState)
+
+    private var _baseUiState = MutableStateFlow<BaseUIState>(BaseUIState.Idle)
+    val baseUiState = _baseUiState.asStateFlow()
+
+    fun updateUiState(uiState: BaseUIState) {
+        _baseUiState.value = uiState
+    }
+
 }
