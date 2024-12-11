dd74b339ead5047522066de9880c47e8995c0d3c
Kirill <99124227+Kupidji@users.noreply.github.com>
2024-12-11 16:58:11 +0500

    Update GenderOption

--- core/common/src/main/java/ru/moonlight/common/GenderOption.kt
+++ core/common/src/main/java/ru/moonlight/common/GenderOption.kt
@@ -4,3 +4,11 @@ enum class GenderOption(val displayName: String) {
     MALE("Мужской"),
     FEMALE("Женский");
 }
+
+fun String.toGenderOptions(): GenderOption {
+    return when (this.lowercase()) {
+        "male" -> GenderOption.MALE
+        "female" -> GenderOption.FEMALE
+        else -> throw IllegalArgumentException("Unknown gender: $this")
+    }
+}
